038460227dc8ab241c0a686a040cd4ad029ab79a
unknown <ma.ebrahimzadeh@gmail.com>
2023-03-07 23:47:06 +0330

    adding project spring

---
+++ spring/src/main/java/ir/mapsa/secondspring/Converters/CourseConverter.java
@@ -0,0 +1,36 @@
+package ir.mapsa.secondspring.Converters;
+
+import ir.mapsa.secondspring.models.Course;
+import ir.mapsa.secondspring.models.CourseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CourseConverter implements BaseConverter<CourseDto, Course> {
+
+    @Override
+    public Course convertDto(CourseDto d)  {
+        Course e = new Course();
+        e.setId(d.getId());
+        e.setVersion(d.getVersion());
+        e.setInsertTimestamp(d.getInsertTimestamp());
+        e.setLastUpdateTimeStamp(d.getLastUpdateTimeStamp());
+        e.setName(d.getName());
+        e.setTopic(d.getTopic());
+        e.setUnit(d.getUnit());
+        return e;
+    }
+
+    @Override
+    public CourseDto convertEntity(Course e) {
+        CourseDto d = new CourseDto();
+        d.setId(e.getId());
+        d.setVersion(e.getVersion());
+        d.setInsertTimestamp(e.getInsertTimestamp());
+        d.setLastUpdateTimeStamp(e.getLastUpdateTimeStamp());
+        d.setName(e.getName());
+        d.setTopic(e.getTopic());
+        d.setUnit(e.getUnit());
+        return d;
+    }
+
+}
