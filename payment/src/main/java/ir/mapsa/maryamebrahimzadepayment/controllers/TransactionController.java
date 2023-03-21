8243d833d26d2fe585d2ab1b72a39d077772b8d7
unknown <ma.ebrahimzadeh@gmail.com>
2023-03-22 03:51:05 +0430

    updating TransactionController and adding new controller

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/controllers/TransactionController.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/controllers/TransactionController.java
@@ -1,10 +1,10 @@
 package ir.mapsa.maryamebrahimzadepayment.controllers;
 
-import ir.mapsa.maryamebrahimzadepayment.converters.TransactionConverter;
 import ir.mapsa.maryamebrahimzadepayment.dto.TransactionDto;
 import ir.mapsa.maryamebrahimzadepayment.exceptions.ServiceException;
 import ir.mapsa.maryamebrahimzadepayment.models.Transaction;
-import ir.mapsa.maryamebrahimzadepayment.services.TransactionService;
+import ir.mapsa.maryamebrahimzadepayment.services.CardTransactionService;
+import ir.mapsa.maryamebrahimzadepayment.services.CommonBaseTransaction;
 import ir.mapsa.maryamebrahimzadepayment.services.TransferLocator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfer")
-public class TransactionController extends AbstractController<Transaction, TransactionDto, TransactionService> {
+public class TransactionController extends AbstractController<Transaction, TransactionDto, CardTransactionService> {
     @Autowired
     private TransferLocator transferLocator;
 
