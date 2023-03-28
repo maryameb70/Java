5433eea021561ed210b0c1b4831376273f1ab9fb
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-03-28 15:52:35 +0430

    add->observe

---
+++ Design Patterns/src/main/java/org/mapsa/behavioral/observer/NewsChannel.java
@@ -0,0 +1,18 @@
+package org.mapsa.behavioral.observer;
+
+public class NewsChannel implements Channel{
+    private String news;
+
+    public String getNews() {
+        return news;
+    }
+
+    public void setNews(String news) {
+        this.news = news;
+    }
+
+    @Override
+    public void update(Object o) {
+
+    }
+}
