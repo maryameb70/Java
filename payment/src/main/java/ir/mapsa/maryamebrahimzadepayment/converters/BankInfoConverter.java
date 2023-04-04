0399c6f270f6d254c6bd708d2604fa32163367a3
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-04-04 22:30:37 +0330

    edit->Mapping

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/BankInfoConverter.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/BankInfoConverter.java
@@ -1,11 +1,21 @@
 package ir.mapsa.maryamebrahimzadepayment.converters;
 
 import ir.mapsa.maryamebrahimzadepayment.dto.BankInfoDto;
+import ir.mapsa.maryamebrahimzadepayment.exceptions.ServiceException;
 import ir.mapsa.maryamebrahimzadepayment.models.BankInfo;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface BankInfoConverter extends BaseConverter<BankInfoDto, BankInfo> {
+    @Mapping(source = "d.branchCode",target = "branch.branchCode")
+    @Mapping(source = "d.accountTypeName",target = "accountType.name")
+    @Mapping(source = "d.customerId",target = "customer.customerId")
+    BankInfo convertDto(BankInfoDto d) throws ServiceException;
+    @Mapping(source = "branch.branchCode",target = "branchCode")
+    @Mapping(source = "accountType.name",target = "accountTypeName")
+    @Mapping(source = "customer.customerId",target = "customerId")
+    BankInfoDto convertEntity(BankInfo e);
 //    @Autowired
 //    private BranchService branchService;
 //    @Autowired
--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/CustomerConverter.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/CustomerConverter.java
@@ -1,12 +1,26 @@
 package ir.mapsa.maryamebrahimzadepayment.converters;
 
+import ir.mapsa.maryamebrahimzadepayment.dto.BankInfoDto;
+import ir.mapsa.maryamebrahimzadepayment.exceptions.ServiceException;
+import ir.mapsa.maryamebrahimzadepayment.models.BankInfo;
 import ir.mapsa.maryamebrahimzadepayment.models.Customer;
 import ir.mapsa.maryamebrahimzadepayment.dto.CustomerDto;
+import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
-public interface CustomerConverter extends BaseConverter<CustomerDto,Customer> {
-//    @Autowired
+public interface CustomerConverter extends BaseConverter<CustomerDto, Customer> {
+    //    @Autowired
 //    private BankInfoConverter bankInfoConverter;
+    @Mapping(source = "branch.branchCode",target = "branchCode")
+    @Mapping(source = "accountType.name",target = "accountTypeName")
+    @Mapping(source = "customer.customerId",target = "customerId")
+    BankInfoDto bankInfoToBankInfoDto(BankInfo bankInfo);
+    @Mapping(source = "bankInfoDto.branchCode",target = "branch.branchCode")
+    @Mapping(source = "bankInfoDto.accountTypeName",target = "accountType.name")
+    @Mapping(source = "bankInfoDto.customerId",target = "customer.customerId")
+    BankInfo bankInfoDtoToBankInfo(BankInfoDto bankInfoDto);
 
 }
