0724b4c454905086dd158aa2424aa7d64650c806
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-04-06 01:16:28 +0330

    edit->method saveTransaction

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/services/CommonBaseTransaction.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/services/CommonBaseTransaction.java
@@ -17,8 +17,8 @@ public abstract class CommonBaseTransaction extends AbstractService<TransactionR
 
     protected Transaction saveTransaction(TransactionDto dto, BankInfo cSender, BankInfo cReceiver) {
         Transaction trxEntity = new Transaction();
-        trxEntity.setReceiver(cReceiver.getCustomer());
-        trxEntity.setSender(cSender.getCustomer());
+        trxEntity.setReceiver(cReceiver);
+        trxEntity.setSender(cSender);
         trxEntity.setDate(new Date());
         trxEntity.setAmount(dto.getAmount());
         repository.save(trxEntity);
