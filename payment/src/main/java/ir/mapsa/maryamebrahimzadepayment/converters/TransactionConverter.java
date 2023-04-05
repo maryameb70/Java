131b712d95bf1cd954dffafe7ea67a72d1144ad7
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-04-06 01:14:52 +0330

    edit->TransactionConverter

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/TransactionConverter.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/TransactionConverter.java
@@ -1,12 +1,21 @@
 package ir.mapsa.maryamebrahimzadepayment.converters;
 
 import ir.mapsa.maryamebrahimzadepayment.dto.TransactionDto;
+import ir.mapsa.maryamebrahimzadepayment.exceptions.ServiceException;
 import ir.mapsa.maryamebrahimzadepayment.models.Transaction;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface TransactionConverter extends BaseConverter<TransactionDto, Transaction> {
-//    @Autowired
+    @Mapping(source = "d.source", target = "sender.cardNumber")
+    @Mapping(source = "d.destination", target = "receiver.cardNumber")
+    Transaction convertDto(TransactionDto d) throws ServiceException;
+
+    @Mapping(source = "sender.cardNumber", target = "source")
+    @Mapping(source = "receiver.cardNumber", target = "destination")
+    TransactionDto convertEntity(Transaction e);
+    //    @Autowired
 //    private BankInfoRepository bankInfo;
 
 
