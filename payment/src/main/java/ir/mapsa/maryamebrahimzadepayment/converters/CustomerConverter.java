09a39ba9a69e70ca0baefee0690a928b3409ab27
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-04-24 03:58:23 +0330

    mapstruct->CustomConverter

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/CustomerConverter.java
+++ payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/CustomerConverter.java
@@ -1,19 +1,14 @@
 package ir.mapsa.maryamebrahimzadepayment.converters;
 
 import ir.mapsa.maryamebrahimzadepayment.dto.BankInfoDto;
-import ir.mapsa.maryamebrahimzadepayment.exceptions.ServiceException;
 import ir.mapsa.maryamebrahimzadepayment.models.BankInfo;
 import ir.mapsa.maryamebrahimzadepayment.models.Customer;
 import ir.mapsa.maryamebrahimzadepayment.dto.CustomerDto;
-import org.mapstruct.InheritInverseConfiguration;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface CustomerConverter extends BaseConverter<CustomerDto, Customer> {
-    //    @Autowired
-//    private BankInfoConverter bankInfoConverter;
     @Mapping(source = "branch.branchCode",target = "branchCode")
     @Mapping(source = "accountType.name",target = "accountTypeName")
     @Mapping(source = "customer.customerId",target = "customerId")
