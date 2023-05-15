774bb061ca5cd7736d23492f3ac3cc6d54b55a63
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-05-15 13:07:11 +0330

    edit->merchantResponse

--- telepardaz/src/main/java/com/example/telepardaz/services/PersonMerchantService.java
+++ telepardaz/src/main/java/com/example/telepardaz/services/PersonMerchantService.java
@@ -6,6 +6,7 @@ import com.example.telepardaz.models.Merchant;
 import com.example.telepardaz.models.PersonMerchant;
 import com.example.telepardaz.repositories.PersonMerchantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,30 +14,23 @@ public class PersonMerchantService extends BaseService<PersonMerchantRepository,
     @Autowired
     private LinkService linkService;
 
+    @Value("$base-url")
+    private String baseUrl;
+
     public MerchantResponse createPersonMerchant(PersonMerchant personMerchant) throws ServiceException {
-        return getMerchantResponse(savePersonMerchant(personMerchant));
+        return getMerchantResponse((PersonMerchant) savePersonMerchant(personMerchant));
     }
 
-    private MerchantResponse getMerchantResponse(Merchant merchant) {
+    private MerchantResponse getMerchantResponse(PersonMerchant merchant) {
         MerchantResponse response = new MerchantResponse();
         response.setId(merchant.getId());
-//        response.setFirstName(merchant.getFirstName());
-//        response.setLastName(merchant.getLastName());
-        response.setUrl("localhost:8080/link?code=" + merchant.getCode());
+        response.setName(merchant.getFirstName());
+        response.setUrl(baseUrl + "/link?code=" + merchant.getCode());
         return response;
     }
 
     private Merchant savePersonMerchant(PersonMerchant personMerchant) throws ServiceException {
-        PersonMerchant entityPerson = new PersonMerchant();
-        entityPerson.setFirstName(personMerchant.getFirstName());
-        entityPerson.setLastName(personMerchant.getLastName());
-        entityPerson.setPhone(personMerchant.getPhone());
-        entityPerson.setMobile(personMerchant.getMobile());
-        entityPerson.setEmail(personMerchant.getEmail());
-        entityPerson.setWebsite(personMerchant.getWebsite());
-        entityPerson.setIban(personMerchant.getIban());
-        entityPerson.setCode(personMerchant.getCode());
-        PersonMerchant savedNewMerchant = repository.save(entityPerson);
+        PersonMerchant savedNewMerchant = repository.save(personMerchant);
         savedNewMerchant.setCode(linkService.generateLink(savedNewMerchant));
         super.update(savedNewMerchant);
         return savedNewMerchant;
