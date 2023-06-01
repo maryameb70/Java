f8ba4a0a471407539556ad89dde2a45c02a98aa6
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-06-01 14:06:19 +0330

    delete demo

--- demo/src/main/java/com/example/demo/Main.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo;
-
-public class Main {
-    public static void main(String[] args) {
-        SingleTone singleTone = SingleTone.getInstance();
-        SingleTone2 singleTone2=SingleTone2.INSTANCE;
-    }
-}
