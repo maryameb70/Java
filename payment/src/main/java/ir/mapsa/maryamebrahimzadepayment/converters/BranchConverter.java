56879e4190c56bb2aecc248c4c1ba618ba39b46d
maryameb70 <ma.ebrahimzadeh@gmail.com>
2023-06-01 18:28:44 +0330

    update

--- payment/src/main/java/ir/mapsa/maryamebrahimzadepayment/converters/BranchConverter.java
+++
@@ -1,9 +0,0 @@
-package ir.mapsa.maryamebrahimzadepayment.converters;
-
-import ir.mapsa.maryamebrahimzadepayment.dto.BranchDto;
-import ir.mapsa.maryamebrahimzadepayment.models.Branch;
-import org.mapstruct.Mapper;
-@Mapper(componentModel = "spring")
-public interface BranchConverter extends BaseConverter<BranchDto, Branch> {
-
-}
