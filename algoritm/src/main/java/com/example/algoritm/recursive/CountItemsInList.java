5422fd099b4be3006a4b875fee7bd93e44bd0f54
maryamebrahimzade <ma.ebrahimzadeh@gmail.com>
2023-06-11 01:25:28 +0330

    add->recursive

--- algoritm/src/main/java/com/example/algoritm/recursive/CountItemsInList.java
+++ algoritm/src/main/java/com/example/algoritm/recursive/CountItemsInList.java
@@ -1,26 +1,26 @@
 package com.example.algoritm.recursive;
 
 public class CountItemsInList {
-    public static Integer count(int length) {
+    public static Integer recursiveCount(int length) {
         if (length == 1) {
             return 1;
         } else {
-            return count(length - 1) + 1;
+            return recursiveCount(length - 1) + 1;
         }
     }
 
-    public static Integer count(int[] array, int length) {
+    public static Integer recursiveCount(int[] array, int length) {
         if (length == 0) {
             return 0;
         } else {
-            return count(array, length - 1) + 1;
+            return recursiveCount(array, length - 1) + 1;
         }
     }
 
     public static void main(String[] args) {
         int[] array = {1, 33, 3, 2, 6, 7, 4, 5, 6};
-        System.out.println("Count the number of items in a list:" + CountItemsInList.count(array.length));
-        System.out.println("Count the number of items in a list:"+CountItemsInList.count(array, array.length));
+        System.out.println("Count the number of items in a list:" + recursiveCount(array.length));
+        System.out.println("Count the number of items in a list:" + recursiveCount(array, array.length));
     }
 }
 
--- algoritm/src/main/java/com/example/algoritm/recursive/Factorial.java
+++ algoritm/src/main/java/com/example/algoritm/recursive/Factorial.java
@@ -1,15 +1,15 @@
 package com.example.algoritm.recursive;
 
 public class Factorial {
-    private static int recursiveFact(int number) {
+    private static int factRecursive(int number) {
         if (number == 1) {
             return 1;
         }
-        return number * recursiveFact(number - 1);
+        return number * factRecursive(number - 1);
     }
 
     public static void main(String[] args) {
         int num = 5;
-        System.out.println("Factorial of " + num + " is: " + recursiveFact(num));
+        System.out.println("Factorial of " + num + " is: " + factRecursive(num));
     }
 }
