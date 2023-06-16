c4e816e6daaab3c7a9c0a8fc31305f68ae21c99c
maryamebrahimzade <ma.ebrahimzadeh@gmail.com>
2023-06-16 16:34:32 +0330

    add->waite

---
+++ core/thread/src/main/java/org/mapsa/interthreadcommunication/examplewaite/WaitSleepSimilaritiesExample.java
@@ -0,0 +1,22 @@
+package org.mapsa.interthreadcommunication.examplewaite;
+
+public class WaitSleepSimilaritiesExample {
+    private static Object obj = new Object();
+
+    public static void main(String[] args)throws InterruptedException
+    {
+        //pause process for two second
+        Thread.sleep(2000);
+
+        System.out.println( Thread.currentThread().getName() +
+                " Thread is woken after two second");
+
+        //create synchronized context from which we call Wait() method
+        synchronized (obj)
+        {
+            //use wait() method to set obj in waiting state for two seconds
+            obj.wait(2000);
+            System.out.println(obj + " Object is in waiting state and woken after 2 seconds");
+        }
+    }
+}
